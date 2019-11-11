9506b36493a3a51aca54ff96e624b24924170c8b
Aaron Arnold <pape.aaron97@icloud.com>
2019-11-10 20:53:16 -0600

    Update main and index

--- main.js
+++ main.js
@@ -1,10 +1,1 @@
-
-
-function setup() {
-    createCanvas(1000, 500);
-    background(228, 229, 240);
-}
-
-function draw() {
-    background(228, 229, 240);
-}
+//Please place code below this line.
